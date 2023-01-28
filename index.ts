fe31c72e75f2be4fff8ff7b81cf08002c28f97bf
Gustavo <u202211458@upc.edu.pe>
2023-01-27 22:48:45 -0500

    login

---
+++ index.ts
@@ -0,0 +1,47 @@
+import express, { Express, Request, Response } from "express"
+import dotenv from "dotenv"
+import axios from 'axios'
+//import morgan from "morgan"
+//import path from "path"
+//import rfs from "rotating-file-stream"
+dotenv.config()
+
+const app: Express = express()
+const port = process.env.SERVER_PORT || 3000
+const loginService = process.env.LOGIN_URL_PROD || ""
+
+/*
+//crear rotation write stream
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotacion diaria
+  path: path.join(__dirname, "log"),
+})
+
+app.use(morgan("combined", { stream: accessLogStream }))
+*/
+
+app.use(express.json())
+
+app.get("/:username/:detail", (req: Request, res: Response) => {
+  const data = {
+    username: req.params.username,
+    detail: req.params.detail,
+  }
+  res.status(200).send(data)
+})
+
+app.post("/login", async (req: Request, res: Response) => {
+  try {
+    const response = await axios.post(loginService, req.body)
+    console.log(response)
+    if (response.data.resultado === 1) {
+      res.status(200).send(response.data.dato)
+    }
+  } catch (error) {
+    res.status(401).send("Error en las credenciales")
+  }
+})
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}!`)
+})
---
+++ out/index.js
@@ -0,0 +1,56 @@
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+const axios_1 = __importDefault(require("axios"));
+//import morgan from "morgan"
+//import path from "path"
+//import rfs from "rotating-file-stream"
+dotenv_1.default.config();
+const app = (0, express_1.default)();
+const port = process.env.SERVER_PORT || 3000;
+const loginService = process.env.LOGIN_URL_PROD || "";
+/*
+//crear rotation write stream
+const accessLogStream = rfs.createStream("access.log", {
+  interval: "1d", // rotacion diaria
+  path: path.join(__dirname, "log"),
+})
+
+app.use(morgan("combined", { stream: accessLogStream }))
+*/
+app.use(express_1.default.json());
+app.get("/:username/:detail", (req, res) => {
+    const data = {
+        username: req.params.username,
+        detail: req.params.detail,
+    };
+    res.status(200).send(data);
+});
+app.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const response = yield axios_1.default.post(loginService, req.body);
+        console.log(response);
+        if (response.data.resultado === 1) {
+            res.status(200).send(response.data.dato);
+        }
+    }
+    catch (error) {
+        res.status(401).send("Error en las credenciales");
+    }
+}));
+app.listen(port, () => {
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}!`);
+});
