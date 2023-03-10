a4784682ccf18410ac966681e6026746ab0572f9
Gustavo <gyauri1300@gmail.com>
2023-03-10 13:52:31 -0500

    cambios server app

--- index.ts
+++ index.ts
@@ -1,92 +1,51 @@
 import express, { Express, Request, Response } from "express"
 import dotenv from "dotenv"
 import axios from "axios"
-//import morgan from "morgan"
-//import path from "path"
-//import rfs from "rotating-file-stream"
 dotenv.config()
 
 const app: Express = express()
+const router = express.Router()
 const port = process.env.SERVER_PORT || 3000
-const loginService = process.env.LOGIN_URL_PROD || ""
-const niubizCredentials = process.env.NIUBIZ_CREDENTIALS_DEV || ""
-const niubizAuthService = process.env.NIUBIZ_SEGURIDAD_DEV || ""
-const niubizPinHashService = process.env.NIUBIZ_CERTIFICADO_DEV || ""
-const niubizConsultaService = process.env.NIUBIZ_CONSULTA_DEV || ""
+const env = process.env.ENVIRONMENT || "DEV"
 
-/*NIUBIZ_CTF_DEV
-//crear rotation write stream
-const accessLogStream = rfs.createStream("access.log", {
-  interval: "1d", // rotacion diaria
-  path: path.join(__dirname, "log"),
-})
-
-app.use(morgan("combined", { stream: accessLogStream }))
-*/
+const niubizAuthService = (env === "PROD" ? process.env.NIUBIZ_SEGURIDAD_PROD : process.env.NIUBIZ_SEGURIDAD_DEV) || ""
+const niubizPinHashService = (env === "PROD" ? process.env.NIUBIZ_CERTIFICADO_PROD : process.env.NIUBIZ_CERTIFICADO_DEV) || ""
+const niubizConsultaService = (env ? process.env.NIUBIZ_CONSULTA_PROD : process.env.NIUBIZ_CONSULTA_DEV) || ""
+const niubizCredentials = (env ? process.env.NIUBIZ_CREDENCIALES_PROD : process.env.NIUBIZ_CREDENCIALES_DEV) || ""
 
 app.use(express.json())
 
-app.post("/login", async (req: Request, res: Response) => {
+router.post("/auth", async (req: Request, res: Response)=> {  
   try {
-    const response = await axios.post(loginService, req.body)
-    console.log(response)
-    if (response.data.resultado === 1) {
-      res.status(200).send({
-        resultado: 1,
-        token: response.data.dato.token,
-        mensaje: "Inicio de sesión exitoso",
-      })
-    }
-  } catch (error) {
-    res.status(200).send({
-      resultado: 0,
-      //token: "",
-      mensaje: "Error en las credenciales de acceso",
-    })
-  }
-})
-
-
-app.post("/niubiz-auth", async (req: Request, res: Response)=> {
-  console.log("auth")
-  try {
-    const response = await axios.post(niubizAuthService, null, { headers: {"Authorization" : "Basic "+ niubizCredentials}})
-    console.log(response.data)    
+    const response = await axios.post(niubizAuthService, null, { headers: {"Authorization" : "Basic "+ niubizCredentials}})    
     res.status(200).send({
       resultado: 1,
       token: response.data,
       })    
-  } catch (error) {
-    console.log(error)    
+  } catch (error) {    
       res.status(200).send({
-      resultado: 0,
-      //token: "",
+      resultado: 0,      
       mensaje: "Error en la generación de token Niubiz",
     })    
   }
 })
 
-app.post("/niubiz-ph", async (req: Request, res: Response)=> {
-  console.log("ph")
+router.post("/host", async (req: Request, res: Response)=> {  
   try {
-    const response = await axios.post(niubizPinHashService, null, { headers: {"Authorization" : req.body.token}})
-    console.log(response.data)    
+    const response = await axios.post(niubizPinHashService, null, { headers: {"Authorization" : req.body.token}})    
     res.status(200).send({
       resultado: 1,
       data: response.data,
       })    
-  } catch (error) {
-    console.log(error)    
+  } catch (error) {    
       res.status(200).send({
-      resultado: 0,
-      //token: "",
+      resultado: 0,      
       mensaje: "Error en la generación de pinHash Niubiz",
     })    
   }
 })
 
-app.get("/niubiz-consulta/:token/:idpago", async (req: Request, res : Response)=>{
-  console.log("consulta")
+router.get("/consulta/:token/:idpago", async (req: Request, res : Response)=>{  
   try {
     const response = await axios.get(niubizConsultaService.toString() +"/"+req.params.idpago, { headers: {"Authorization" : req.params.token}})    
     res.status(200).send({
@@ -94,15 +53,15 @@ app.get("/niubiz-consulta/:token/:idpago", async (req: Request, res : Response)=
       data: response.data.order,
       })    
       
-  } catch (error) {
-    console.log(error)    
+  } catch (error) {    
       res.status(200).send({
-      resultado: 0,
-      //token: "",
+      resultado: 0,      
       mensaje: "Error en la consulta de transacción Niubiz",
     })
   }
 })
+
+app.use("/uapapp/api/niubiz", router)
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`)
--- out/index.js
+++ out/index.js
@@ -15,88 +15,47 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const axios_1 = __importDefault(require("axios"));
-//import morgan from "morgan"
-//import path from "path"
-//import rfs from "rotating-file-stream"
 dotenv_1.default.config();
 const app = (0, express_1.default)();
+const router = express_1.default.Router();
 const port = process.env.SERVER_PORT || 3000;
-const loginService = process.env.LOGIN_URL_PROD || "";
-const niubizAuthService = process.env.NIUBIZ_SEGURIDAD_DEV || "";
-const niubizCredentials = process.env.NIUBIZ_CREDENTIALS_DEV || "";
-const niubizPinHashService = process.env.NIUBIZ_CTF_DEV || "";
-const niubizCodigoComercio = process.env.NIUBIZ_CODIGO_COMERCIO || "";
-const niubizConsultaService = process.env.NIUBIZ_CONSULTA_DEV || "";
-/*NIUBIZ_CTF_DEV
-//crear rotation write stream
-const accessLogStream = rfs.createStream("access.log", {
-  interval: "1d", // rotacion diaria
-  path: path.join(__dirname, "log"),
-})
-
-app.use(morgan("combined", { stream: accessLogStream }))
-*/
+const env = process.env.ENVIRONMENT || "DEV";
+const niubizAuthService = (env === "PROD" ? process.env.NIUBIZ_SEGURIDAD_PROD : process.env.NIUBIZ_SEGURIDAD_DEV) || "";
+const niubizPinHashService = (env === "PROD" ? process.env.NIUBIZ_CERTIFICADO_PROD : process.env.NIUBIZ_CERTIFICADO_DEV) || "";
+const niubizConsultaService = (env ? process.env.NIUBIZ_CONSULTA_PROD : process.env.NIUBIZ_CONSULTA_DEV) || "";
+const niubizCredentials = (env ? process.env.NIUBIZ_CREDENCIALES_PROD : process.env.NIUBIZ_CREDENCIALES_DEV) || "";
 app.use(express_1.default.json());
-app.post("/login", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    try {
-        const response = yield axios_1.default.post(loginService, req.body);
-        console.log(response);
-        if (response.data.resultado === 1) {
-            res.status(200).send({
-                resultado: 1,
-                token: response.data.dato.token,
-                mensaje: "Inicio de sesión exitoso",
-            });
-        }
-    }
-    catch (error) {
-        res.status(200).send({
-            resultado: 0,
-            //token: "",
-            mensaje: "Error en las credenciales de acceso",
-        });
-    }
-}));
-app.post("/niubiz-auth", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("auth");
+router.post("/auth", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield axios_1.default.post(niubizAuthService, null, { headers: { "Authorization": "Basic " + niubizCredentials } });
-        console.log(response.data);
         res.status(200).send({
             resultado: 1,
             token: response.data,
         });
     }
     catch (error) {
-        console.log(error);
         res.status(200).send({
             resultado: 0,
-            //token: "",
             mensaje: "Error en la generación de token Niubiz",
         });
     }
 }));
-app.post("/niubiz-ph", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("ph");
+router.post("/host", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield axios_1.default.post(niubizPinHashService, null, { headers: { "Authorization": req.body.token } });
-        console.log(response.data);
         res.status(200).send({
             resultado: 1,
             data: response.data,
         });
     }
     catch (error) {
-        console.log(error);
         res.status(200).send({
             resultado: 0,
-            //token: "",
             mensaje: "Error en la generación de pinHash Niubiz",
         });
     }
 }));
-app.get("/niubiz-consulta/:token/:idpago", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    console.log("consulta");
+router.get("/consulta/:token/:idpago", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     try {
         const response = yield axios_1.default.get(niubizConsultaService.toString() + "/" + req.params.idpago, { headers: { "Authorization": req.params.token } });
         res.status(200).send({
@@ -105,14 +64,13 @@ app.get("/niubiz-consulta/:token/:idpago", (req, res) => __awaiter(void 0, void 
         });
     }
     catch (error) {
-        console.log(error);
         res.status(200).send({
             resultado: 0,
-            //token: "",
             mensaje: "Error en la consulta de transacción Niubiz",
         });
     }
 }));
+app.use("/uapapp/api/niubiz", router);
 app.listen(port, () => {
     console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
